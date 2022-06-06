830ec135f1bfa24366f437ff66e86bb8b0317f1c
mtedeschini <matias.tedeschini@globant.com>
2022-06-06 10:03:48 -0300

    adding scoop test

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import ScoopOption from './ScoopOption';
+
+export default function Options(optionType) {
+
+    const [items, setItems] = useState([])
+    
+    useEffect(() => {
+        axios
+          .get(`http://localhost:3030/${optionType}`)
+          .then((response) => setItems(response.data))
+          .catch((error) => {});
+      }, [optionType]);
+
+    // TODO: reemplazar 'null' con ToppingOptions
+    const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+    const optionItems = items.map((item) => (
+        <ItemComponent
+          key={item.name}
+          name={item.name}
+          imagePath={item.imagePath}
+        />
+      ));
+    
+    return (
+        <Row>{optionItems}</Row>
+    )
+}
